ff99f150bf795d8aba6885417952cc7799a1206a
Hari krishnan P U <harikrishnan9a@gmail.com>
2025-09-08 10:54:26 +0530

    factory method - creational design pattern

---
+++ factory-method/factory.js
@@ -0,0 +1,59 @@
+
+class Document { //INTERFACE
+    constructor(content) {
+        this.content = content;
+    }
+
+    render() {
+        throw new Error("render must implement");
+    }
+}
+
+class PdfDocument extends Document { // CONCRETE PRODUCTS
+    render() {
+        console.log(`rendering PDF: "${this.content}"`);
+    }
+}
+
+class WordDocument extends Document {
+    render() {
+        console.log(`rendering Word file : "${this.content}"`);
+    }
+}
+
+
+class DocumentCreator{ // CREATOR INTERFACE
+
+    createDocument(){
+        throw new Error("createDoc must implement");
+    }
+
+    openDocument(content){
+        const doc = this.createDocument(content);
+        doc.render();
+    }
+
+}
+
+// CONCRETE CREATORS
+
+class PdfCreator extends DocumentCreator {
+    createDocument(content) {
+        return new PdfDocument(content);
+    }
+}
+
+class WordCreator extends DocumentCreator {
+    createDocument(content) {
+        return new WordDocument(content);
+    }
+}
+
+const pdfCreator = new PdfCreator();
+pdfCreator.openDocument("Annual Report 2024");
+
+const wordCreator = new WordCreator();
+wordCreator.openDocument("Meeting Minutes");
+
+
+
